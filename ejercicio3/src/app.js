51de826b2fedb431c1dff370270c6f378e3ae7d0
unknown <cristianlanza@gmail.com>
2023-07-20 14:48:45 -0300

    try-catch

--- ejercicio3/src/app.js
+++ ejercicio3/src/app.js
@@ -4,9 +4,14 @@ const ProductManager = require('./productManager');
 const pM = new ProductManager;
 
 const arrayProducts = async () => {  
-  const products = await pM.getProducts();
-  products.splice(0, 1);
-  return products;
+  try {
+    const products = await pM.getProducts();
+    products.splice(0, 1);
+    return products;
+    
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const port = 8080;
@@ -14,20 +19,30 @@ const port = 8080;
 const app = express();
 
  app.get("/products", async (req, res) => {
-  const products = await arrayProducts();
-  const { limit } = req.query;
-  const productFilter = products.slice(0, limit || 10);
-  res.json({ message: productFilter });
+  try {
+    const products = await arrayProducts();
+    const { limit } = req.query;
+    const productFilter = products.slice(0, limit || 10);
+    res.json({ message: productFilter });
+    
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get( '/products/:id', async ( req, res ) => {
-  const products = await arrayProducts();
-  const { id } = req.params;
-  const product = products.find(p => p.id === Number(id));
-  if(product) {
-    return res.json({ message: product });
+  try {
+    const products = await arrayProducts();
+    const { id } = req.params;
+    const product = products.find(p => p.id === Number(id));
+    if(product) {
+      return res.json({ message: product });
+    }
+    res.json({ message: 'El producto no existe.'})
+    
+  } catch (error) {
+    console.log(error);
   }
-  res.json({ message: 'El producto no existe.'})
 })
 
 app.listen( port, () => {
